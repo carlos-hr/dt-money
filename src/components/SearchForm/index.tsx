d4432c205aee77ddff5d23b1b4cc6be71a57c180
Carlos-hr <carlos.henrique17.chr@gmail.com>
2022-08-22 16:18:48 -0300

    feat(forms): creating transaction form

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,11 +1,37 @@
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
 import { SearchFormContainer } from "./styled";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+const searchFormSchema = zod.object({
+  query: zod.string(),
+});
+
+type SearchFormInput = zod.infer<typeof searchFormSchema>;
 
 const SearchForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormSchema),
+  });
+
+  async function onSearchTransactions(data: SearchFormInput) {
+    await new Promise((res) => setTimeout(res, 2000));
+    console.log(data);
+  }
+
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
-      <button type="submit">
+    <SearchFormContainer onSubmit={handleSubmit(onSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register("query")}
+      />
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>

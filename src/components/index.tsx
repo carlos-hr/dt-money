895b59b77d9dbc8afe8d993a003321fe2bf82da6
Carlos-hr <carlos.henrique17.chr@gmail.com>
2022-08-22 11:28:37 -0300

    feat(modal): creating transaction modal

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header";
 export { default as Summary } from "./Summary";
 export { default as SearchForm } from "./SearchForm";
+export { default as NewTransactionModal } from "./NewTransactionModal";
